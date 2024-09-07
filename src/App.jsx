e1a6afddc827595e21c1e12871ac348fba94f3f8
twani <jbestius@gmail.com>
2024-09-07 19:13:13 +0200

    add the linear horizontal animation

--- src/App.jsx
+++ src/App.jsx
@@ -29,16 +29,16 @@ function App() {
           <p className="py-7 max-w-[1000px] w-full  mx-auto text-slate-500 text-center text-lg font-medium">
             This is a simple React application that implements infinite horizontal card scrolling ♾️. When the user hovers over the component, the scrolling pauses offering a seamless and efficient browsing experience 🎨.
           </p>
-          <div className="flex flex-1 gap-y-3 overflow-x-scroll">
-            {users.map((user, index) => (
-              <div key={index} className=" items-center bg-slate-800 mx-2 rounded-xl w-[400px]">
-                <div className="flex flex-col items-start justify-start p-4rounded-lg m-4">
+          <div className="flex flex-1 gap-y-3 pt-[4rem] animate-[horizontalScroll_80s_ease_infinite] w-fit">
+            {users.slice(1,10).map((user, index) => (
+              <div key={index} className=" items-center bg-slate-800 mx-2 rounded-xl">
+                <div className="flex flex-col items-start justify-start p-4rounded-lg m-4 w-[400px]">
                   <div className='flex flex-col'>
                   <img src="https://xsgames.co/randomusers/avatar.php?g=pixel" alt={user.username} className="w-10 h-10 rounded-full" />
-                  <small>{user.username}</small>
+                  <small className="text-slate-600">{user.username}</small>
                   </div>
                   <h2 className="text-xl text-slate-100 font-semibold mt-4">{`${user.first_name} ${user.last_name}`}</h2>
-                  <p className="text-base text-slate-500 font-normal mt-2">{user.comment}</p>
+                  <p className="text-base text-slate-400 font-normal mt-2">{user.comment}</p>
                 </div>
               </div>
             ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ export default {
         'auto-fill-400': 'repeat(auto-fill, minmax(400px, 1fr))',
         'auto-fit-100': 'repeat(auto-fit, minmax(100px, 1fr))',
       },
+      keyframes: {
+        horizontalScroll:{
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      },
+      animation:{
+        horizontalScroll:"horizontalScroll 80s linear infinite"
+      }
     },
   },
   plugins: [],
